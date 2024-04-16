9e9f4b0706243608463708fbc954e503d02f03f9
Amit-kumar-verma <97842674+akv2511@users.noreply.github.com>
2024-04-16 19:09:58 +0530

    Update User.model.js

--- server/models/User.model.js
+++ server/models/User.model.js
@@ -46,6 +46,6 @@ userSchema.pre("save", function (next) {
   next();
 });
 
-const User = mongoose.model("User", userSchema);
+const User = mongoose.model("Client", userSchema);
 
-module.exports = User;
+module.exports = Client;
